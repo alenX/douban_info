2d4aa0c75535de2080c3348f0c02e8896516bbc6
wangss <genhaoai@gmail.com>
2016-12-22 23:05:39 +0800

    sql格式化

--- init.sql
+++ init.sql
@@ -1,7 +1,18 @@
-create table douban_book
-(id INT(32) PRIMARY KEY AUTO_INCREMENT,name VARCHAR(64),
-  author VARCHAR(32),county varchar(32),translator VARCHAR(32),press VARCHAR(32),
-  data_str VARCHAR(32),price DECIMAL(6,2),socre FLOAT,description TEXT,image VARCHAR(128));
-
-
-CREATE table douban_book_tag(id INT(32) PRIMARY KEY AUTO_INCREMENT,tag_name VARCHAR(32))
+CREATE TABLE douban_book
+(
+  id          INT(32) PRIMARY KEY AUTO_INCREMENT,
+  name        VARCHAR(64),
+  author      VARCHAR(32),
+  county      VARCHAR(32),
+  translator  VARCHAR(32),
+  press       VARCHAR(32),
+  data_str    VARCHAR(32),
+  price       DECIMAL(6, 2),
+  socre       FLOAT,
+  description TEXT,
+  image       VARCHAR(128)
+);
+CREATE TABLE douban_book_tag (
+  id       INT(32) PRIMARY KEY AUTO_INCREMENT,
+  tag_name VARCHAR(32)
+)
